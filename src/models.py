0591e1d096ff185c39150956b5e034ca744b9324
sathiya06 <sathiyavenkat06@gmail.com>
2023-10-18 23:44:55 -0400

    Feat:  Added product table to add wish listed products

--- src/models.py
+++ src/models.py
@@ -9,7 +9,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 
-'''Creating a database model for users and todos. '''
+'''Creating a database model for users. '''
 ''' The __tablename__ attribute is used to define the name of the table in the database. '''
 
 '''
@@ -40,5 +40,24 @@ class Users(Base):
     first_name = Column(String)
     last_name = Column(String)
     hashed_password = Column(String)
+    # list = relationship('Products', back_populates='users')
 
-    # todos = relationship("Todos", back_populates="owner")
+'''
+    The user table has the following columns:
+    Columns
+    ----------
+    product_id: int
+        the id of the product
+    user_id: int
+        the id of the user
+    product:str
+        the product of the user
+'''
+
+class Products(Base):
+    __tablename__ = "products"
+
+    product_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'), index=True)
+    product = Column(String)
+    # user = relationship('Users', back_populates='products')
